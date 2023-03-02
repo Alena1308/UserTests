c3972fb8828ab03dc04981f74505fddc90c841ac
Alena1308 <Lenamironova13@mail.ru>
2023-03-02 11:01:45 +0300

    Initial commit

---
+++ src/main/java/sky/me/usertests/UserTestsApplication.java
@@ -0,0 +1,13 @@
+package sky.me.usertests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/me/usertests/UserTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.me.usertests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
